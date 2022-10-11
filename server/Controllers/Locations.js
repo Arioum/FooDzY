a3d6682bdf225d63feefa9b6d625aa774bc0f632
“[arioum]” <[amoghrao121@gmail.com]>
2022-10-11 20:19:53 +0530

    added .env .gitignore configs

---
+++ server/Controllers/Locations.js
@@ -0,0 +1,17 @@
+const Locations = require("../models/Locations");
+
+exports.getAllLocations = (req, res) => {
+  Locations.find()
+    .then((success) => {
+      res.status(200).json({
+        message: "successfully fetched",
+        Locations: success,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "someting went wrong",
+        Error: err,
+      });
+    });
+};
