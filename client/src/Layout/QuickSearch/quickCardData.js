ab77c87d47e56ffb38853611cfd82960e39114d6
“[arioum]” <[amoghrao121@gmail.com]>
2022-10-27 20:02:35 +0530

    added query

--- client/src/Layout/QuickSearch/quickCardData.js
+++ client/src/Layout/QuickSearch/quickCardData.js
@@ -3,37 +3,37 @@ const quickCardData = [
         imageURL : "/Images/1x1/breakfast.png",
         cardTitle : "Breakfast",
         cardDesc : "Start your day with exclusive breakfast options",
-        path : "/search&category=breakfast"
+        path : "/search?category=breakfast"
     },
     {
         imageURL : "/Images/1x1/lunch.png",
         cardTitle : "Lunch",
         cardDesc : "Have a mid day fill with our lunch varieties",
-        path : "/search&category=lunch"
+        path : "/search?category=lunch"
     },
     {
         imageURL : "/Images/1x1/snacks.png",
         cardTitle : "Snacks",
         cardDesc : "Crunch on some mouth watering snacks",
-        path : "/search&category=snacks"
+        path : "/search?category=snacks"
     },
     {
         imageURL : "/Images/1x1/dinner.png",
         cardTitle : "Dinner",
         cardDesc : "End your day with a positive yet delicious note",
-        path : "/search&category=dinner"
+        path : "/search?category=dinner"
     },
     {
         imageURL : "/Images/1x1/drinks.png",
         cardTitle : "Drinks",
         cardDesc : "Quench your thirst with a variety of beverages",
-        path : "/search&category=drinks"
+        path : "/search?category=drinks"
     },
     {
         imageURL : "/Images/1x1/nightlife.png",
         cardTitle : "Nightlife",
         cardDesc : "Vent your stress with your friends and enjoy your time",
-        path : "/search&category=nightlife"
+        path : "/search?category=nightlife"
     }
 ]
 
